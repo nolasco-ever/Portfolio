bae5cc621b1beba7eb369c3700a682e771d08973
Ever Nolasco <nolasco.ever19@gmail.com>
2021-03-11 21:54:57 -0500

    Header Created
    The header has been created. It takes up 100% height and width and consists of a logo. And instructs the user to scroll down at the bottom of the page

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Ever Nolasco</title>
+		<link rel="stylesheet" href="CSS/style.css">
+	</head>
+
+	<header>
+		<img id="logo" src="logos/white-1.png" alt="Ever Nolasco Logo">
+		<p>Scroll down</p>
+		<img id="arrow" src="icons/down-arrow.png" alt="down arrow">
+	</header>
+</html>
