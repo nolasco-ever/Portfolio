69e046ba6cf94a3a2ad57beaed24986d881f0637
Ever Nolasco <nolasco.ever19@gmail.com>
2021-03-24 22:04:29 -0400

    Uploading functions created
    A separate php file, upload-project.php, has been created that will upload the information inputted in the form. This will take that information and insert it into the Projects table in the Portfolio database.
    
    Bug:
    While the destination of the images are uploaded to the database, the images are not seen in the actual uploads folder.

---
+++ includes/upload-project.php
@@ -0,0 +1,103 @@
+<?php
+	require 'dbh.inc.php';	//Connect to the database
+
+	if(isset($_POST['upload'])){
+		// Set up variables based on the inputs from the form on upload-page.php
+		$title = $_POST['title'];
+		$desc = $_POST['desc'];
+		$git = $_POST['git-link'];
+		$web = $_POST['web-link'];
+
+		$img1_name = $_FILES['img1']['name'];
+		$img1_tmp = $_FILES['img1']['tmp_name'];
+		$img1_size = $_FILES['img1']['size'];
+
+		$img2_name = $_FILES['img2']['name'];
+		$img2_tmp = $_FILES['img2']['tmp_name'];
+		$img2_size = $_FILES['img2']['size'];
+
+		$img3_name = $_FILES['img3']['name'];
+		$img3_tmp = $_FILES['img3']['tmp_name'];
+		$img3_size = $_FILES['img3']['size'];
+
+		$category = $_POST['category'];
+
+		// Variables to hold destination of compressed image
+		$img1_comp = compressed($img1_name, $img1_tmp);
+		$img2_comp = compressed($img2_name, $img2_tmp);
+		$img3_comp = compressed($img3_name, $img3_tmp);
+
+		// Insert values into the SQL Projects table
+		$sql = "INSERT INTO Projects(title, description, github, website, img1, img2, img3, category) VALUES (?,?,?,?,?,?,?,?)";
+		$stmt = mysqli_stmt_init($conn);
+
+		if(!mysqli_stmt_prepare($stmt, $sql)){
+			header("Location: ../upload-page.php?error=sqlerror");
+			exit();
+		}
+		else{
+			mysqli_stmt_bind_param($stmt, "ssssssss", $title, $desc, $git, $web, $img1_comp, $img2_comp, $img3_comp, $category);
+			mysqli_stmt_execute($stmt);
+
+			header("Location: ../upload-page.php?upload=success");
+			exit();
+		}
+	}
+	else{
+		header("Location: ../upload-page.php");
+		exit();
+	}
+
+	// Function that will gather file info and pass it to compressImage() function
+	function compressed($img_name, $img_tmp){
+  		// File info 
+  		$uploadPath = "uploads/";
+  		$imageUploadPath = $uploadPath.$img_name; 
+  		$fileType = pathinfo($imageUploadPath, PATHINFO_EXTENSION);
+  		 
+  		// Allow certain file formats 
+  		$allowTypes = array('jpg','png','jpeg'); 
+  		if(in_array($fileType, $allowTypes)){
+  		    // Compress size and upload image 
+  		    $compressedImage = compressImage($img_tmp, $imageUploadPath, 75); 
+  		     
+  		    if($compressedImage){ 
+  		        $compressedImageSize = filesize($compressedImage);
+  		        move_uploaded_file($img_tmp, $compressedImage);
+  		        return $compressedImage;	//Return the compressed image destination
+  		    }else{ 
+  		        $statusMsg = "Image compress failed!"; 
+  		    } 
+  		}else{ 
+  		    $statusMsg = 'Sorry, only JPG, JPEG & PNG files are allowed to upload.'; 
+  		}
+	}
+
+	// Function that will compress image and return destination
+	function compressImage($source, $destination, $quality) { 
+	    // Get image info 
+	    $imgInfo = getimagesize($source); 
+	    $mime = $imgInfo['mime']; 
+	     
+	    // Create a new image from file 
+	    switch($mime){ 
+	        case 'image/jpeg': 
+	            $image = imagecreatefromjpeg($source); 
+	            break; 
+	        case 'image/png': 
+	            $image = imagecreatefrompng($source); 
+	            break; 
+	        case 'image/gif': 
+	            $image = imagecreatefromgif($source); 
+	            break; 
+	        default: 
+	            $image = imagecreatefromjpeg($source); 
+	    } 
+	     
+	    // Save image 
+	    imagejpeg($image, $destination, $quality); 
+	     
+	    // Return compressed image 
+	    return $destination; 
+	} 
+?>
--- upload-page.php
+++ upload-page.php
@@ -6,26 +6,33 @@
 	</head>
 
 	<body>
-		<div>
-			<form id="upload-form" action="">
-				<input id="title" type="text" placeholder="Project Title">
-				<textarea id="description" name="Project-Description" cols="50" rows-"4" placeholder="Project Description"></textarea>
-				<input id="github-link" type="text" placeholder="Github Link">
-				<input id="web-link" type="text" placeholder="Website Link (if applicable)">
-				<input id="img1" type="file" name="img1">
-				<input id="img2" type="file" name="img2">
-				<input id="img3" type="file" name="img3">
+		<form id="upload-form" action="includes/upload-project.php" method="post" enctype="multipart/form-data">
+			<input name="title" id="title" type="text" placeholder="Project Title">
+			<textarea name="desc" id="description" name="Project-Description" cols="50" rows-"4" placeholder="Project Description"></textarea>
+			<input name="git-link" id="github-link" type="text" placeholder="Github Link">
+			<input name="web-link" id="web-link" type="text" placeholder="Website Link (if applicable)">
+			<input name="img1" id="img1" type="file" name="img1">
+			<input name="img2" id="img2" type="file" name="img2">
+			<input name="img3" id="img3" type="file" name="img3">
+			<h2 id="radio-title">Category:</h2>
+			<input type="radio" id="mod-sim" name="category" value="Modeling and Simulation">
+			<label id="mod-sim-label" for="Modeling and Simulation">Modeling and Simulation</label>
+			<input type="radio" id="web-dev" name="category" value="Web Development">
+			<label id="web-dev-label" for="Web Development">Web Development</label>
+			<input type="radio" id="game-dev" name="category" value="Game Development">
+			<label id="game-dev-label" for="Game Development">Game Development</label>
+			<input id="submit-button" type="submit" value="Upload" name="upload">
+		</form>
 
-				<h2 id="radio-title">Category:</h2>
-				<input type="radio" id="mod-sim" name="category" value="Modeling and Simulation">
-				<label id="mod-sim-label" for="Modeling and Simulation">Modeling and Simulation</label>
-				<input type="radio" id="web-dev" name="category" value="Web Development">
-				<label id="web-dev-label" for="Web Development">Web Development</label>
-				<input type="radio" id="game-dev" name="category" value="Game Development">
-				<label id="game-dev-label" for="Game Development">Game Development</label>
+		<table>
+				<tr>
+					<th colspan="2"><h2>Projects</h2></th>
+				</tr>
 
-				<input id="submit-button" type="submit" value="Upload">
-			</form>
-		</div>
+				<tr>
+					<td class="proj-name"><p>Pinicle: Social Media Application (Web)</p></td>
+					<td class="delete"><form action=""><input class="delete-button" type="submit" value="Delete"></form></td>
+				</tr>
+			</table>
 	</body>		
 </html>
